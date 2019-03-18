1fcfebda5194ce58482a32bbd853ed7c330bba89
guptaankit894 <48688147+guptaankit894@users.noreply.github.com>
2019-03-18 21:31:50 +0530

    Add files via upload

---
+++ test1.m
@@ -0,0 +1,89 @@
+load('ex3data1.mat');
+ %ONEVSALL trains multiple logistic regression classifiers and returns all 
+ %the classifiers in a matrix all_theta, where the i-th row of all_theta 
+ %corresponds to the classifier for label i
+ %   [all_theta] = ONEVSALL(X, y, num_labels, lambda) trains num_labels
+ %   logistic regression classifiers and returns each of these classifiers
+ %   in a matrix all_theta, where the i-th row of all_theta corresponds 
+ %   to the classifier for label i
+
+ % Some useful variables
+ m = size(X, 1);
+ n = size(X, 2);
+ num_labels=10;
+ lambda=0.1;
+ % You need to return the following variables correctly 
+ all_theta = zeros(num_labels, n + 1);
+ J=zeros(num_labels,1);
+ % Add ones to the X data matrix
+ X = [ones(m, 1) X];
+
+     % Set Initial theta
+     initial_theta = zeros(n + 1, 1);
+	 % Set options for fminunc
+     options = optimset('GradObj', 'on', 'MaxIter', 50);
+	
+	
+	
+for i=1:num_labels
+ theta = zeros(size(all_theta,2),1);
+ y==i;
+ 
+ z=X*theta;
+ predictor=1./(1+exp(-z));
+
+ theta=((1/m)*(X'*(predictor-y)))+((lambda/m)*theta);
+ 
+ theta(1)=theta(1)-((lambda/m)*theta(1));
+ 
+ J=((y'*log(predictor))+((1-y)'*log(1-predictor)));
+ J=-J/m;
+ 
+ J=J+(sum(theta(2:end).^2)*(lambda/(2*m)));
+ 
+ all_theta=all_theta+theta(:)';
+ end
+ 
+ 
+for i=1:num_labels
+ %     % Run fmincg to obtain the optimal theta
+ %     % This function will return theta and the cost 
+     [theta] = ...
+         fmincg (@(t)(lrCostFunction(t, X, (y == i), lambda)), ...
+                 initial_theta, options);
+end
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should complete the following code to train num_labels
+%               logistic regression classifiers with regularization
+%               parameter lambda. 
+%
+% Hint: theta(:) will return a column vector.
+%
+% Hint: You can use y == c to obtain a vector of 1's and 0's that tell you
+%       whether the ground truth is true/false for this class.
+%
+% Note: For this assignment, we recommend using fmincg to optimize the cost
+%       function. It is okay to use a for-loop (for c = 1:num_labels) to
+%       loop over the different classes.
+%
+%       fmincg works similarly to fminunc, but is more efficient when we
+%       are dealing with large number of parameters.
+%
+% Example Code for fmincg:
+%
+%     % Set Initial theta
+%     initial_theta = zeros(n + 1, 1);
+%     
+%     % Set options for fminunc
+ %    options = optimset('GradObj', 'on', 'MaxIter', 50);
+% 
+%     % Run fmincg to obtain the optimal theta
+%     % This function will return theta and the cost 
+%   [theta] = ...
+%      fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+%             initial_theta, options);
+%
+
+
+
